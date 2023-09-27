9a823de2c4a8ddb87cdecac8aa5ac5670e15f21b
BenKTAMU <145497372+BenKTAMU@users.noreply.github.com>
2023-09-27 12:02:23 -0500

    Become GitHub Professional

--- Assets/Scripts/GorillaController.cs
+++ Assets/Scripts/GorillaController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class GorillaController : MonoBehaviour
 {
+    [SerializeField] private float movementSpeed = 2;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    private float jpower;
+    private float timer;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        isGrounded = false;
+        Debug.Log("Start");
+        rb = GetComponent<Rigidbody2D>();
+        rb.mass = 0.5f;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag.Equals("Platform"))
+        {
+            isGrounded = true;
+            Debug.Log("Grounded");
+        }
+        Debug.Log("Collision");
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * movementSpeed * 3 * Time.deltaTime, 0.0f, 0.0f);
+
+
+        /*
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            isGrounded = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 9.0f, ForceMode2D.Impulse);
+        }
+        */
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            timer = Time.time;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && isGrounded)
+        {
+
+            isGrounded = false;
+            Debug.Log(Time.time - timer);
+            jpower = Time.time - timer;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * (jpower * 8), ForceMode2D.Impulse);
+        }
+
     }
 }
