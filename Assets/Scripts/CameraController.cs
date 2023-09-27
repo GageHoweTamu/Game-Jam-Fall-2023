b08c5862c2c9fddac96484b438274ae075f08539
zacharyshewbarttamu <zacharyshewbart@tamu.edu>
2023-09-26 22:42:49 -0500

    Camera Update with Rooms
    Updated camera so that it is based on rooms rather than just following the player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,9 +10,13 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
 
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
 
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
     }
 
 
