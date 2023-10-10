79493d1184df74805119d5de5456681627398122
Lannpha <44383929+Lannpha@users.noreply.github.com>
2023-10-10 03:59:37 -0500

    animations, airborne tilt code
    some of the animations are like 60fps and then some of them are literally just one frame

--- Assets/Scripts/ParasiteMovement.cs
+++ Assets/Scripts/ParasiteMovement.cs
@@ -18,7 +18,17 @@ public class PlayerController3 : MonoBehaviour
     public bool controlling;
     private SpriteRenderer sprite;
     private Rigidbody2D rb;
+    //animator vars
     private Animator anim;
+    private Transform anim_child;
+    public float idleWalkThresholdSpeed;
+    public float walkRunThresholdSpeed;
+    public float walkProportionalAnimSpeed;
+    private bool jump;
+    private bool fall;
+    private bool squash;
+    public Vector3 scale;
+
 
     // Start is called before the first frame update
     private void Start()
@@ -27,7 +37,9 @@ public class PlayerController3 : MonoBehaviour
         controlling = false;
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
-        anim = GetComponent<Animator>();
+        anim = GetComponentsInChildren<Animator>()[1];
+        anim_child = transform.Find("anim_child");
+        jump = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -67,7 +79,6 @@ public class PlayerController3 : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-
         if (controlling == true)
         {
             if (GameObject.FindWithTag("Player").name == gorillaName) 
@@ -102,34 +113,73 @@ public class PlayerController3 : MonoBehaviour
         else
         {
             Flip();
+            //ANIMATION CONTROLS
+            anim.SetBool("grounded", isGrounded);
+            jump = !isGrounded && rb.velocity.y > 0;
+            anim.SetBool("jump",jump);
+            squash = (Input.GetAxis("Vertical") < 0.0f && isGrounded && Mathf.Abs(rb.velocity.x) < idleWalkThresholdSpeed);
+            anim.SetBool("squash",squash);
+            /* cool idea but sprites are origined on their center so this causes floating
+            if(squash)
+            {
+                float new_scale_y = Mathf.Max(transform.localScale.y - 0.5f*Time.deltaTime, scale.y*0.5f);
+                transform.localScale = new Vector3(transform.localScale.x, new_scale_y, transform.localScale.z);
+            } else 
+            {
+                float new_scale_y = Mathf.Min(transform.localScale.y + 0.5f*Time.deltaTime, scale.y);
+                transform.localScale = new Vector3(transform.localScale.x, new_scale_y, transform.localScale.z);
+            }
+            */
 
             if (isGrounded) // grounded
             {
-                anim.speed = Mathf.Abs(rb.velocity.x * 0.5f);
+                //ANIMATION CONTROLS
+                float speed = Mathf.Abs(rb.velocity.x);
+                anim.SetFloat("anim_speed_mult", Mathf.Abs(rb.velocity.x) * walkProportionalAnimSpeed);
+                anim.SetBool("walk", speed >= idleWalkThresholdSpeed);
+                anim.SetBool("run", speed >= walkRunThresholdSpeed);
+
                 if (Input.GetAxis("Horizontal") != 0.0f)
                 {
                     rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
                     // gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0.0f, 0.0f);
                     // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0.0f, 0.0f), ForceMode2D.Impulse);
                 }
+                anim_child.rotation = Quaternion.Euler(0.0f,0.0f,0.0f);
             }
             else // not grounded
             {
-                //FINISH!!!
-                // anim.state = 
+                //-- ROTATION WHILE IN AIR
+                //angle of attack
+                if(rb.velocity.y != 0)
+                {    
+                    float alpha = 1.0f * Mathf.Rad2Deg*Mathf.Atan(rb.velocity.y / rb.velocity.x);
+                    float tiltAroundZ = 1.0f * ((System.Convert.ToSingle(alpha >= 0.0f) - 0.5f)*2.0f) * Mathf.Pow(Mathf.Abs(alpha),0.9f);
+                    //
+                    RaycastHit2D hit;
+                    hit = Physics2D.Raycast (transform.position, Vector2.down, Mathf.Infinity);
+                    if(hit.collider != null && hit.distance - 0.75f <= 1.75f && rb.velocity.y < 0)
+                    {
+                        Quaternion target = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+                        anim_child.rotation = Quaternion.Slerp(anim_child.rotation, target,  Time.deltaTime * 4.0f);
+                    } else
+                    {
+                        Quaternion target = Quaternion.Euler(0, 0, tiltAroundZ);
+                        anim_child.rotation = Quaternion.Slerp(anim_child.rotation, target,  Time.deltaTime * 2.0f);
+                    }
+                }
                 if (Input.GetAxis("Horizontal") != 0.0f)
                 {
                     rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed * 2.0f, rb.velocity.y);
+                    
                     // gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * movementSpeed * 2 * Time.deltaTime, 0.0f, 0.0f);
                     // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0.0f, 0.0f), ForceMode2D.Impulse);
                 }
             }
 
-
-
             if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             {
-
+                //JUMP
                 isGrounded = false;
                 gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 9.0f, ForceMode2D.Impulse);
             }
